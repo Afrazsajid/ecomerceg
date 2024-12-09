b0deb14941d8b4f44a15da48831ec6291afe329f
Afrazsajid <afrazsajid55@gmail.com>
2024-12-09 13:55:28 +0500

    adding footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+ import Footer from "@/components/footer"
 
 
 
@@ -34,6 +34,7 @@ export default function RootLayout({
       >
         
         {children}
+        <Footer/>
       </body>
 
     </html>
